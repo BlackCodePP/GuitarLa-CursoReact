9bc94c709857aa629a3b9b878cfa789bc8618704
Pedro Panisello <pedropanisello23@gmail.com>
2024-06-18 12:06:14 +0200

    Add products to the cart with quantity

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,19 @@ import { db } from "./data/db";
 
 export default function App() {
   const [data, setData] = useState(db);
+  const [cart, setCart] = useState([]);
 
-  console.log(data);
+  function addToCart(item) {
+    const itemExist = cart.findIndex((guitar) => guitar.id === item.id);
+    if (itemExist >= 0) {
+      const updatedCart = [...cart];
+      updatedCart[itemExist].quantity++;
+      setCart(updatedCart);
+    } else {
+      item.quantity = 1;
+      setCart([...Cart, item]);
+    }
+  }
 
   return (
     <>
@@ -16,8 +27,13 @@ export default function App() {
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-          {data.map(() => (
-            <Guitar price={100} />
+          {data.map((guitar) => (
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}
+              setCart={setCart}
+              addToCart={addToCart}
+            />
           ))}
         </div>
       </main>
--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,21 +1,24 @@
-export default function Guitar(props) {
+export default function Guitar({ guitar, addToCart }) {
+  const { id, name, image, description, price } = guitar;
+
   return (
     <div className="col-md-6 col-lg-4 my-4 row align-items-center">
       <div className="col-4">
         <img
           className="img-fluid"
-          src="./public/img/guitarra_01.jpg"
+          src={`/img/${image}.jpg`}
           alt="imagen guitarra"
         />
       </div>
       <div className="col-8">
-        <h3 className="text-black fs-4 fw-bold text-uppercase">Lukather</h3>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae
-          labore odit magnam in autem nesciunt, amet deserunt
-        </p>
-        <p className="fw-black text-primary fs-3">${props.price}</p>
-        <button type="button" className="btn btn-dark w-100">
+        <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
+        <p>{description}</p>
+        <p className="fw-black text-primary fs-3">${price}</p>
+        <button
+          type="button"
+          className="btn btn-dark w-100"
+          onClick={() => addToCart(guitar)}
+        >
           Agregar al Carrito
         </button>
       </div>
