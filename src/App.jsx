6dfb9f1ccc39978242c2e2bdc4d30a05d5cdadfa
Pedro Panisello <pedropanisello23@gmail.com>
2024-06-20 16:22:35 +0200

    add functions increaseQuantity, decreaseQuantity and removeFromCart

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,60 @@ export default function App() {
   const [data, setData] = useState(db);
   const [cart, setCart] = useState([]);
 
+  const MIN_ITEMS = 1;
+  const MAX_ITEMS = 5;
+
   function addToCart(item) {
     const itemExist = cart.findIndex((guitar) => guitar.id === item.id);
     if (itemExist >= 0) {
+      if (cart[itemExist].quantity >= MAX_ITEMS) return;
       const updatedCart = [...cart];
       updatedCart[itemExist].quantity++;
       setCart(updatedCart);
     } else {
       item.quantity = 1;
-      setCart([...Cart, item]);
+      setCart([...cart, item]);
     }
+  }
+
+  function removeFromCart(id) {
+    setCart((prevCart) => prevCart.filter((guitar) => guitar.id !== id));
+  }
+
+  function decreaseQuantity(id) {
+    const updatedCart = cart.map((item) => {
+      if (item.id === id && item.quantity > MIN_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity - 1,
+        };
+      }
+      return item;
+    });
+    setCart(updatedCart);
+  }
+
+  function increaseQuantity(id) {
+    const updatedCart = cart.map((item) => {
+      if (item.id === id && item.quantity < MAX_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity + 1,
+        };
+      }
+      return item;
+    });
+    setCart(updatedCart);
   }
 
   return (
     <>
-      <Header />
+      <Header
+        cart={cart}
+        removeFromCart={removeFromCart}
+        increaseQuantity={increaseQuantity}
+        decreaseQuantity={decreaseQuantity}
+      />
 
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
