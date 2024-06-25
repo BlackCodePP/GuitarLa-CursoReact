1ce8e1d238ac4062886852710148a55fb5efaa69
Pedro Panisello <pedropanisello23@gmail.com>
2024-06-25 16:22:36 +0200

    Deployment

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
     return localStorageCart ? JSON.parse(localStorageCart) : [];
   };
 
-  const [data, setData] = useState(db);
+  const [data] = useState(db);
   const [cart, setCart] = useState(initialCart);
 
   const MIN_ITEMS = 1;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,7 +22,7 @@ export default function Header({
             <a href="index.html">
               <img
                 className="img-fluid"
-                src="./public/img/logo.svg"
+                src="/img/logo.svg"
                 alt="imagen logo"
               />
             </a>
@@ -31,7 +31,7 @@ export default function Header({
             <div className="carrito">
               <img
                 className="img-fluid"
-                src="./public/img/carrito.png"
+                src="/img/carrito.png"
                 alt="imagen carrito"
               />
 
